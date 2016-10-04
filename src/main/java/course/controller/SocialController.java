218d07eaa8e9cf444634d0a7cddfd8cdd47f6a6d
Noxoffa <alex-noks@yandex.ru>
2016-10-04 23:59:01 +0300

    Revert "refactor index to thymeleaf"
    This reverts commit 755262339f1b99d5ce2295b62936175bc39220b9.

--- src/main/java/course/controller/SocialController.java
+++ src/main/java/course/controller/SocialController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.inject.Inject;
-import javax.servlet.http.HttpSession;
 
 /**
  * Created by Артем Константинович on 30.09.2016.
@@ -37,9 +36,9 @@ public class SocialController {
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String logging(Model model, HttpSession httpSession) {
-
+    public String logging(Model model) {
         User user = new User();
+
         if ((connectionRepository.findPrimaryConnection(Twitter.class) != null)) {
             if (userRepository.findByUserUrl(twitter.userOperations().getUserProfile().getProfileUrl())==null){
                 user.setName(twitter.userOperations().getScreenName());
@@ -50,14 +49,12 @@ public class SocialController {
                 model.addAttribute("user", user.getUserUrl());
                 model.addAttribute("name", user.getName());
                 model.addAttribute("role", user.getRole());
-                httpSession.setAttribute("role", user.getRole());
                 model.addAttribute("img", twitter.userOperations().getUserProfile().getProfileImageUrl());
             }
             else{
                 model.addAttribute("user",userRepository.findByUserUrl(twitter.userOperations().getUserProfile().getProfileUrl()).getUserUrl());
                 model.addAttribute("name",userRepository.findByUserUrl(twitter.userOperations().getUserProfile().getProfileUrl()).getName());
                 model.addAttribute("role",userRepository.findByUserUrl(twitter.userOperations().getUserProfile().getProfileUrl()).getRole());
-                httpSession.setAttribute("role", userRepository.findByUserUrl(twitter.userOperations().getUserProfile().getProfileUrl()).getRole());
                 model.addAttribute("img", twitter.userOperations().getUserProfile().getProfileImageUrl());
             }
         }
@@ -71,22 +68,17 @@ public class SocialController {
                     model.addAttribute("user",user.getUserUrl());
                     model.addAttribute("name", user.getName());
                     model.addAttribute("role", user.getRole());
-                    httpSession.setAttribute("role", user.getRole());
                     model.addAttribute("img", "http://graph.facebook.com/"+facebook.userOperations().getUserProfile().getId()+"/picture?type=square");
                 }
                 else{
                     model.addAttribute("user",userRepository.findByUserUrl(facebook.userOperations().getUserProfile().getLink()).getUserUrl());
                     model.addAttribute("name",userRepository.findByUserUrl(facebook.userOperations().getUserProfile().getLink()).getName());
                     model.addAttribute("role",userRepository.findByUserUrl(facebook.userOperations().getUserProfile().getLink()).getRole());
-                    httpSession.setAttribute("role",userRepository.findByUserUrl(facebook.userOperations().getUserProfile().getLink()).getRole());
                     model.addAttribute("img", "http://graph.facebook.com/"+facebook.userOperations().getUserProfile().getId()+"/picture?type=square");
                 }
             }
         }
-
         return "index";
     }
-
-
 
 }
