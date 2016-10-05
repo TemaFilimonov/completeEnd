d1ab4c3a4b5e5ce553eeb97f4aa5014decc0af3e
Noxoffa <alex-noks@yandex.ru>
2016-10-06 02:50:42 +0300

    last todo done
    TODO: Send json with list of sites

--- src/main/java/course/controller/SiteController.java
+++ src/main/java/course/controller/SiteController.java
@@ -1,9 +1,21 @@
 package course.controller;
 
+import course.dao.SiteRepository;
+import course.domain.Site;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
 /**
  * Created by Артем Константинович on 06.10.2016.
  */
-/*@Controller
+@Controller
 @RequestMapping("/")
 public class SiteController {
 
@@ -18,9 +30,9 @@ public class SiteController {
 
     @RequestMapping(value = "/site/info/{id}", method = RequestMethod.GET)
     public @ResponseBody
-    Site ViewProfile(HttpSession httpSession, @PathVariable("id") String id){
-        Site site = new Site();
-        site = siteRepository.FindByOwnerId(Integer.parseInt(id));
-        return null;
+    List<Site> ViewSites(HttpSession httpSession, @PathVariable("id") long id){
+        List<Site> sites;
+        sites = siteRepository.findByOwnerId(id);
+        return sites;
     }
-}*/
+}
--- src/main/java/course/dao/SiteRepository.java
+++ src/main/java/course/dao/SiteRepository.java
@@ -1,12 +1,18 @@
 package course.dao;
 
+import course.domain.Site;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 /**
  * Created by Артем Константинович on 06.10.2016.
  */
-/*
+
 @Repository
 public interface SiteRepository extends JpaRepository<Site, Long> {
     Site findById(long id);
-    Site FindByOwnerId (long ownerID);
+    List<Site> findByOwnerId (long ownerId);
 }
-*/
+
