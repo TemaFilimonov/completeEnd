7d26b872d793a66346750dc2f468d271adfd168f
TemaFilimonov <filart18@mail.ru>
2016-10-05 19:57:26 +0300

    new ng controller

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -3,10 +3,17 @@
 
 var App = angular.module('profile', []);
 
-App.controller('profileCtrl', function ($scope, $http) {
+App.controller('profileUserCtrl', function ($scope, $http) {
   $scope.user = [];
     
   $http.get('http://localhost:8080/curentUser/info').success(function (data) {
     $scope.user = data;
+  });
+});
+App.controller('profileSiteCtrl', function ($scope, $http) {
+  $scope.site = [];
+
+  $http.get('http://localhost:8080/someDir').success(function (data) {
+    $scope.site = data;
   });
 });
