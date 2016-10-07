11ef51036ba763d0dbfcc8e429b0b4050cc060fb
Noxoffa <alex-noks@yandex.ru>
2016-10-07 20:37:30 +0300

    add create site view

--- target/classes/static/js/dnd.js
+++ target/classes/static/js/dnd.js
@@ -5,7 +5,7 @@
 /**
  * The controller doesn't do much more than setting the initial data model
  */
-angular.module("dnd").controller("NestedListsDemoController", function($scope) {
+angular.module("dnd", ['dndLists']).controller("sran", function($scope) {
 
     $scope.models = {
         selected: null,
@@ -14,8 +14,108 @@ angular.module("dnd").controller("NestedListsDemoController", function($scope) {
             {type: "container", id: 1, columns: [[], []]}
         ],
         dropzones: {
-        "A": [],
-        "B": []
+            "A": [
+                {
+                    "type": "container",
+                    "id": 1,
+                    "columns": [
+                        [
+                            {
+                                "type": "item",
+                                "id": "1"
+                            },
+                            {
+                                "type": "item",
+                                "id": "2"
+                            }
+                        ],
+                        [
+                            {
+                                "type": "item",
+                                "id": "3"
+                            }
+                        ]
+                    ]
+                },
+                {
+                    "type": "item",
+                    "id": "4"
+                },
+                {
+                    "type": "item",
+                    "id": "5"
+                },
+                {
+                    "type": "item",
+                    "id": "6"
+                }
+            ],
+            "B": [
+                {
+                    "type": "item",
+                    "id": 7
+                },
+                {
+                    "type": "item",
+                    "id": "8"
+                },
+                {
+                    "type": "container",
+                    "id": "2",
+                    "columns": [
+                        [
+                            {
+                                "type": "item",
+                                "id": "9"
+                            },
+                            {
+                                "type": "item",
+                                "id": "10"
+                            },
+                            {
+                                "type": "item",
+                                "id": "11"
+                            }
+                        ],
+                        [
+                            {
+                                "type": "item",
+                                "id": "12"
+                            },
+                            {
+                                "type": "container",
+                                "id": "3",
+                                "columns": [
+                                    [
+                                        {
+                                            "type": "item",
+                                            "id": "13"
+                                        }
+                                    ],
+                                    [
+                                        {
+                                            "type": "item",
+                                            "id": "14"
+                                        }
+                                    ]
+                                ]
+                            },
+                            {
+                                "type": "item",
+                                "id": "15"
+                            },
+                            {
+                                "type": "item",
+                                "id": "16"
+                            }
+                        ]
+                    ]
+                },
+                {
+                    "type": "item",
+                    "id": 16
+                }
+            ]
         }
     };
 
